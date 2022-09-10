e693398cf728ee4db86bf57a00e5b6d05098ef94
TrinhThuc <trinhthuc432@gmail.com>
2022-09-10 09:53:57 +0700

    aksjnansdfa

--- src/main/java/com/example/authenusser/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/example/authenusser/service/impl/CustomUserDetailsService.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private UserRepository userRepository; ///
+    private UserRepository userRepository; //
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
