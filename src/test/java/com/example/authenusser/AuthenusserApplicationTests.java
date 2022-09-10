322675f9f4683ca18e3ddd8d48715779b09287b3
TrinhThuc <trinhthuc432@gmail.com>
2022-09-10 08:48:30 +0700

    1stbuildpj

---
+++ src/test/java/com/example/authenusser/AuthenusserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenusser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenusserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
